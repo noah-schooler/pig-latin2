34528b04f4da8fcc04a28d6e5acce40d0e058901
Noah Schooler <noahschooler1@gmail.com>
2018-12-14 15:06:22 -0800

    refactor for readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,14 @@
-// This code contains two potential bugs: if the letter following "qu" in an initail consonant string is not a vowel, the code will fail; if "y" is the first letter of a word and the letter following "y" is a consonant, the code will fail. The code will correctly return words if the first letter is capitazlied, but cannot handle embedded punctuation or capitalization.
+// This code contains two potential bugs: if the letter following "qu" in an initail consonant string is not a vowel, the code will fail; if "y" is the first letter of a word and the letter following "y" is a consonant, the code will fail. The code will correctly return words if the first letter is capitazlied, but cannot handle embedded punctuation or capitalization. Extra whitespace and punctuation outside of a word are not accounted for.
 
 function translateSentence(sentence) {
-  var words = sentence.toLowerCase().split(" ")
+  var lowercaseWords = sentence.toLowerCase().split(" ")
   var regularWords = sentence.split(" ")
   var pigLatinWords = []
-  words.forEach(function(word) {
+  lowercaseWords.forEach(function(word) {
     pigLatinWords.push(translateWord(word))
   });
   var capitalizedWords = capitalize(regularWords, pigLatinWords)
   return punctuateSentence(capitalizedWords)
-}
-
-function capitalize(english, pigLatin) {
-  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  var counter = 0
-  var newArray = []
-  english.forEach(function(word) {
-    if (uppercaseLetters.includes(word[0])) {
-      newArray.push(pigLatin[counter][0].toUpperCase() + pigLatin[counter].slice(1))
-    } else {
-      newArray.push(pigLatin[counter])
-    }
-    counter += 1
-  });
-  return newArray
-}
-
-function punctuateSentence(sentence) {
-  joinedWords = []
-  sentence.forEach(function(word) {
-    joinedWords.push(punctuateWord(word))
-  });
-  return joinedWords.join(" ")
-}
-
-function punctuateWord(word) {
-  var punctuation = ['.', ',', '?', '!', ':', ';']
-  var endPunc = []
-  var regular = []
-  chars = word.split("")
-  chars.forEach(function(char) {
-    if (punctuation.includes(char)) {
-      endPunc.push(char)
-    } else {
-      regular.push(char)
-    }
-  });
-  return regular.join("") + endPunc.join("")
 }
 
 function translateWord(word) {
@@ -77,16 +39,56 @@ function translateWord(word) {
   return newWord.concat(consonants.join("") + "ay")
 }
 
+function capitalize(regularWords, pigLatinWords) {
+  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var counter = 0
+  var capitalizedWords = []
+  regularWords.forEach(function(word) {
+    if (uppercaseLetters.includes(word[0])) {
+      capitalizedWords.push(pigLatinWords[counter][0].toUpperCase() + pigLatinWords[counter].slice(1))
+    } else {
+      capitalizedWords.push(pigLatinWords[counter])
+    }
+    counter += 1
+  });
+  return capitalizedWords
+}
+
+function punctuateSentence(sentence) {
+  joinedWords = []
+  sentence.forEach(function(word) {
+    joinedWords.push(punctuateWord(word))
+  });
+  return joinedWords.join(" ")
+}
+
+function punctuateWord(word) {
+  var punctuation = ['.', ',', '?', '!', ':', ';']
+  var endPunc = []
+  var strippedWord = []
+  chars = word.split("")
+  chars.forEach(function(char) {
+    if (punctuation.includes(char)) {
+      endPunc.push(char)
+    } else {
+      strippedWord.push(char)
+    }
+  });
+  return strippedWord.join("") + endPunc.join("")
+}
+
 function findVowels(word) {
   var counter = 0
   var vowels = []
   var splitWord = word.split("")
   splitWord.forEach(function(letter) {
+    console.log(vowels);
+    // Why does this evaluate to true for consonants?
     if (letter === "a" || "e" || "i" || "o" || "u") {
       vowels.push(letter)
     } else {
       vowels.push("-")
     }
   });
-  return vowels
+  return vowels.join(" ")
 }
