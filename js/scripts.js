92cd7b95d72db58dfaa061c434954c2c4b0d6d81
Noah Schooler <noahschooler1@gmail.com>
2018-12-14 14:19:45 -0800

    add functions to properly capitalize and punctuate Pig Latin words

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,53 @@
+// This code contains two potential bugs: if the letter following "qu" in an initail consonant string is not a vowel, the code will fail; if "y" is the first letter of a word and the letter following "y" is a consonant, the code will fail. The code will correctly return words if the first letter is capitazlied, but cannot handle embedded punctuation or capitalization.
+
 function translateSentence(sentence) {
   var words = sentence.toLowerCase().split(" ")
+  var regularWords = sentence.split(" ")
   var pigLatinWords = []
   words.forEach(function(word) {
     pigLatinWords.push(translateWord(word))
   });
-  return pigLatinWords.join(" ")
+  var capitalizedWords = capitalize(regularWords, pigLatinWords)
+  return punctuateSentence(capitalizedWords)
 }
 
-// This code contains two potential bugs: if the letter following "qu" in an initail consonant string is not a vowel, the code will fail; if "y" is the first letter of a word and the letter following "y" is a consonant, the code will fail.
+function capitalize(english, pigLatin) {
+  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var counter = 0
+  var newArray = []
+  english.forEach(function(word) {
+    if (uppercaseLetters.includes(word[0])) {
+      newArray.push(pigLatin[counter][0].toUpperCase() + pigLatin[counter].slice(1))
+    } else {
+      newArray.push(pigLatin[counter])
+    }
+    counter += 1
+  });
+  return newArray
+}
 
-var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+function punctuateSentence(sentence) {
+  joinedWords = []
+  sentence.forEach(function(word) {
+    joinedWords.push(punctuateWord(word))
+  });
+  return joinedWords.join(" ")
+}
+
+function punctuateWord(word) {
+  var punctuation = ['.', ',', '?', '!', ':', ';']
+  var endPunc = []
+  var regular = []
+  chars = word.split("")
+  chars.forEach(function(char) {
+    if (punctuation.includes(char)) {
+      endPunc.push(char)
+    } else {
+      regular.push(char)
+    }
+  });
+  return regular.join("") + endPunc.join("")
+}
 
 function translateWord(word) {
   var firstLetter = word.slice(0, 1)
